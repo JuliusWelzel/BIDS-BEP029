05f96735e268af6d804d6200879af84fa1b7fc3d
Julius Welzel <julius.welzel@gmail.com>
2023-05-05 10:05:19 +0200

    [FIX] figures for manuscript

--- paper_plot_python/example_data.py
+++ paper_plot_python/example_data.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # quick plot
-dat = pd.read_table(r"walk.csv", delimiter=';')
+dat = pd.read_table(r"paper_plot_python\walk.csv", delimiter=';')
 
 
 n_sam = len(dat.AccX)
 time = np.linspace(0,n_sam / 100, n_sam)
 peaks, _ = find_peaks(dat.AccZ,height=40)
 
-plt.figure(figsize=(8, 8))
+# setting font sizeto 30
+plt.rcParams.update({'font.size': 30})
+plt.figure(figsize=(15, 13))
 sns.set_palette("rocket")
-sns.lineplot(time, dat.AccX, label='lf_acc_x')
-sns.lineplot(time, dat.AccY, label='lf_acc_z')
-sns.lineplot(time, dat.AccZ, label='lf_acc_z')
+sns.lineplot(time, dat.AccX, label='rf_acc_x')
+sns.lineplot(time, dat.AccY, label='rf_acc_z')
+sns.lineplot(time, dat.AccZ, label='rf_acc_z')
 plt.vlines(time[peaks],-80,80,colors=[0,0,0],linestyles='dotted')
 plt.vlines(time[peaks[0]],-80,80,colors=[0,0,0],linestyles='dotted', label='heel strike')
 plt.xlabel('Time [s]')
